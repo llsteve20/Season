7ff76a8f96637bbf845a8622a13b20f12b23e4ef
Levi Stephen <levisteveen@gmail.com>
2022-04-11 16:37:36 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const App = () => {
-    return(
-        <div>Hi There!</div>
-    );
-};
+class App extends React.Component {
+     state = {lat: null, errorMessage: ''};
 
-ReactDOM.render(
-    <App />, document.querySelector("#root")
+     componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({lat: position.coords.latitude}),
+            (err) => this.setState({errorMessage: err.message})
+        );
+      }
+
+      componentDidUpdate(){
+        console.log('My component was updated - it rerendered');
+      }
+
+    //required
+    render() {
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error:{this.state.errorMessage}</div>;
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>Latitude:{this.state.lat}</div>;
+        }
+
+        return <div>Loading</div>
+     }  
+}
+
+ReactDOM.createRoot(document.querySelector("#root"))
+.render(
+    <App />
 );
