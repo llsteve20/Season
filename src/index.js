d92e0d9b17db7a30774bcd2874c1a91fd6db2481
Levi Stephen <levisteveen@gmail.com>
2022-04-12 11:13:10 +0100

    App completed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
      state = {lat: null, errorMessage: ''};
@@ -17,17 +18,25 @@ class App extends React.Component {
         
       }
 
+
+      renderContent(){
+            if(this.state.errorMessage && !this.state.lat){
+                return <div>Error:{this.state.errorMessage}</div>;
+            }
+
+            if(!this.state.errorMessage && this.state.lat){
+                return <SeasonDisplay lat={this.state.lat} />;
+            }
+
+            return <Spinner message='Please accept Location request' />;
+      }
     //required
     render() {
-        if(this.state.errorMessage && !this.state.lat){
-            return <div>Error:{this.state.errorMessage}</div>;
-        }
-
-        if(!this.state.errorMessage && this.state.lat){
-            return <div><SeasonDisplay lat={this.state.lat} /></div>;
-        }
-
-        return <div>Loading</div>
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
      }  
 }
 
